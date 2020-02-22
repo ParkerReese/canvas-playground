0cd2c58a98f7f7aa3f6b6dd6f6032e252170735f
ParkerReese <preese@vendasta.com>
2020-02-22 17:44:28 -0600

    clicking drawns black line from bottom middle of page

--- src/app/lines/lines.component.ts
+++ src/app/lines/lines.component.ts
@@ -1,5 +1,4 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {Circle} from "../moving-circles/circle";
 
 @Component({
   selector: 'app-lines',
@@ -7,21 +6,41 @@ import {Circle} from "../moving-circles/circle";
   styleUrls: ['./lines.component.scss']
 })
 export class LinesComponent implements OnInit {
-@ViewChild('canvas', {static: true})
+  @ViewChild('canvas', {static: true})
 
   private canvas: ElementRef<HTMLCanvasElement>;
   private ctx: CanvasRenderingContext2D;
 
-  private lines: Circle[] = [];
-
   private colors: string[] = ['#355c7d', '#6c5b7b', '#c06c84', '#f67280'];
-  private radiusOptions: number[] = [8, 16, 24];
 
-  private intervalId: any;
-  private requestedAnimationFrame: number;
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    /* Assign the width and height to stretch the canvas across the available space.
+       This will not get changed on window resizing  */
+    this.ctx.canvas.height = window.innerHeight - 64;
+    this.ctx.canvas.width = window.innerWidth;
+  }
+
+  // Draw line on mouse clck
+  public mouseClick(event: MouseEvent): void {
+    this.drawLine(event.offsetX, event.offsetY)
+  }
+
+  // Draw line given input location and global vars
+  private drawLine(xPos: number, yPos: number) {
+    this.ctx.beginPath();
+    // All lines will be drawn from the bottom middle, for simplicity
+    this.ctx.moveTo(this.ctx.canvas.width / 2, this.ctx.canvas.height,);
+
+    this.ctx.lineTo(xPos, yPos);
+    this.ctx.stroke();
+  }
+
+  public clearCanvas() {
+    this.ctx.clearRect(0, 0, this.ctx.canvas.height, this.ctx.canvas.width);
   }
 
 }
