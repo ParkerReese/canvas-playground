a38cc58a02ac35bd7dca3983255ab6d28cf238ea
ParkerReese <preese@vendasta.com>
2020-02-23 17:57:34 -0600

    fire a laser that does not move

---
+++ src/app/ray-gun/laser.ts
@@ -0,0 +1,30 @@
+export class Laser {
+
+  constructor(private ctx: CanvasRenderingContext2D,
+              private xCoord: number,
+              private yCoord: number,
+  ) {
+    this.draw();
+  }
+
+  public move() {
+    // Update position
+    // Draw new circle
+    this.draw();
+  }
+
+  private draw() {
+    this.ctx.beginPath();
+
+    this.ctx.moveTo(this.xCoord, this.yCoord);
+
+    const linearGradient = this.ctx.createLinearGradient(this.xCoord, this.yCoord, this.xCoord + 100, this.yCoord);
+    linearGradient.addColorStop(0, 'rgba(255,255,255,0)'); // transparent white
+    linearGradient.addColorStop(1, 'rgba(255,0,0,1)'); // transparent white
+    this.ctx.strokeStyle = linearGradient;
+
+    console.log('stroke');
+    this.ctx.lineTo(this.xCoord + 100, this.yCoord);
+    this.ctx.stroke();
+  }
+}
--- src/app/ray-gun/ray-gun.component.ts
+++ src/app/ray-gun/ray-gun.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Laser} from "./laser";
 
 @Component({
   selector: 'ray-gun',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ray-gun.component.scss']
 })
 export class RayGunComponent implements OnInit {
+  @ViewChild('canvas', {static: true})
 
-  constructor() { }
+  private canvas: ElementRef<HTMLCanvasElement>;
+  private ctx: CanvasRenderingContext2D;
+
+  private lasers: Laser[] = [];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    // Canvas
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    /* Assign the width and height to stretch the canvas across the available space.
+       This will not get changed on window resizing  */
+    this.ctx.canvas.height = window.innerHeight - 64;
+    this.ctx.canvas.width = window.innerWidth;
+  }
+
+  // Handle mouse click event on the canvas
+  public mouseClick(event: MouseEvent): void {
+    // Need to offset the image of the ray gun cursor
+    const xStartPos = event.offsetX;
+    const yStartPos = event.offsetY + 9;
+    this.lasers.push(new Laser(this.ctx, xStartPos, yStartPos));
   }
 
 }
