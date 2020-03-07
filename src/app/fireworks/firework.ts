8b220a020b6e24cd5e6f112ea691b2fe3fa49c55
ParkerReese <preese@vendasta.com>
2020-03-07 12:05:47 -0600

    draw a multi-colored laser to the click location

---
+++ src/app/fireworks/firework.ts
@@ -0,0 +1,83 @@
+export interface Coordinate {
+  x: number;
+  y: number;
+}
+
+export class Firework {
+  // Current actual position
+  private curPosition: Coordinate;
+  // Used to create a tail
+  private pastPositions: Coordinate[] = [];
+  private pastPositionsLength: number = 4;
+  // Angle between the bottom of the canvas and the path of the firework to the target
+  private angle: number;
+  // Movement
+  private speed: number = 1.5;
+  private acceleration: number = 1.05;
+
+  constructor(private ctx: CanvasRenderingContext2D,
+              // Starting position
+              private startPos: Coordinate,
+              // Ending position
+              private targetPos: Coordinate
+  ) {
+    // Initialize past positions
+    for (let i = 0; i < this.pastPositionsLength; i++) {
+      this.pastPositions.push({x: startPos.x, y: startPos.y});
+    }
+    this.curPosition = {x: startPos.x, y: startPos.y};
+    this.angle = Math.atan2(targetPos.y - startPos.y, targetPos.x - startPos.x);
+
+    this.draw();
+  }
+
+  public update() {
+    this.move();
+    this.draw();
+  }
+
+  // Move firework to next position
+  private move() {
+    // remove last item in coordinates array
+    this.pastPositions.pop();
+    // add current coordinates to the start of the array
+    this.pastPositions.unshift({x: this.curPosition.x, y: this.curPosition.y});
+    // speed up the firework
+    this.speed *= this.acceleration;
+    // get the current velocities based on angle and speed
+    const vx = Math.cos(this.angle) * this.speed;
+    const vy = Math.sin(this.angle) * this.speed;
+    // assign next position given current position and new speed
+    this.curPosition.x += vx;
+    this.curPosition.y += vy;
+  }
+
+  // Draw firework given latest position
+  private draw() {
+    this.ctx.beginPath();
+    // move to the last tracked coordinate in the set, then draw a line to the current x and y
+    const pastPos = this.pastPositions[this.pastPositionsLength - 1];
+    this.ctx.moveTo(pastPos.x, pastPos.y);
+    this.ctx.lineTo(this.curPosition.x, this.curPosition.y);
+    this.ctx.strokeStyle = this.randColor();
+    this.ctx.stroke();
+  }
+
+  // returns a random hsl color https://www.w3schools.com/colors/colors_hsl.asp
+  private randColor(): string {
+    return 'hsl(' + this.random(0, 360) + ', 100%, ' + this.random(50, 60) + '%)';
+  }
+
+  // get a random number within a range
+  private random(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  public get currentPos(): Coordinate {
+    return this.curPosition
+  }
+
+  public get target(): Coordinate {
+    return this.targetPos;
+  }
+}
--- src/app/fireworks/fireworks.component.ts
+++ src/app/fireworks/fireworks.component.ts
@@ -1,17 +1,22 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, NgZone, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Firework } from "./firework";
 
 @Component({
   selector: 'app-fireworks',
   templateUrl: './fireworks.component.html',
   styleUrls: ['./fireworks.component.scss']
 })
-export class FireworksComponent implements OnInit {
+export class FireworksComponent implements OnInit, OnDestroy {
   @ViewChild('canvas', {static: true})
 
   private canvas: ElementRef<HTMLCanvasElement>;
   private ctx: CanvasRenderingContext2D;
+  private intervalId: any;
+  private requestedAnimationFrame: number;
 
-  constructor() {
+  private fireworks: Firework[] = [];
+
+  constructor(private ngZone: NgZone) {
   }
 
   ngOnInit(): void {
@@ -21,9 +26,55 @@ export class FireworksComponent implements OnInit {
        This will not get changed on window resizing  */
     this.ctx.canvas.height = window.innerHeight - 64;
     this.ctx.canvas.width = window.innerWidth;
+
+    // Used to not fire change detection when animating
+    this.ngZone.runOutsideAngular(() => this.animate());
+
+    // Call the animate() function ever interval in ms
+    this.intervalId = setInterval(() => {
+      this.animate();
+    }, 15);
+  }
+
+  // Move the circles given their initialized values
+  private animate(): void {
+    this.requestedAnimationFrame = requestAnimationFrame(() => this.animate);
+
+    /* Draw the background */
+    this.clearCanvas();
+
+    // ensure compositionOperation for our fireworks and particles
+    // lighter creates bright highlight points as the fireworks and particles overlap each other
+    this.ctx.globalCompositeOperation = 'lighter';
+
+    // Move and draw each firework
+    this.fireworks.forEach((firework, index) => {
+      // Delete fireworks that have hit their destination
+      if (firework.currentPos.y <= firework.target.y) {
+        this.fireworks.splice(index, 1);
+        return;
+      }
+      firework.update();
+    });
+  }
+
+  private clearCanvas(): void {
+    // setting the composite operation to destination-out will allow us to clear the canvas at a specific opacity, rather than wiping it entirely
+    this.ctx.globalCompositeOperation = 'destination-out';
+    // decrease the alpha property to create more prominent trails
+    this.ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
+    this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
   }
 
   // Handle mouse click event on the canvas
   public mouseClick(event: MouseEvent): void {
+    const startPosition = {x: this.ctx.canvas.width / 2, y: this.ctx.canvas.height};
+    const target = {x: event.offsetX, y: event.offsetY};
+    this.fireworks.push(new Firework(this.ctx, startPosition, target))
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+    cancelAnimationFrame(this.requestedAnimationFrame);
   }
 }
